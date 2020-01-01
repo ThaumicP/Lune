535c9a6568b9848e65d0b38937c2a2f5e68b5275
ThaumicP <thaumicp@gmail.com>
2020-01-01 09:42:33 +0400

    Using Premake

--- Lune/src/Lune/EntryPoint.h
+++ Lune/src/Lune/EntryPoint.h
@@ -8,8 +8,7 @@ int main(int argc, char** argv)
 {
 	Lune::Log::Init();
 	LN_CORE_WARN("Initialized Core Log");
-	int a = 5;
-	LN_INFO("Hello! Var={0}", a);
+	LN_INFO("Hello!");
 
 	auto app = Lune::CreateApplication();
 	app->Run();
